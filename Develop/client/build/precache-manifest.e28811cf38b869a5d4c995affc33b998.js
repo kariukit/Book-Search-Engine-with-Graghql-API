b391d3debe9ecd6a753f0b9178f5de05e6bb64c5
kariukit <nyawirakariukit@gmail.com>
2022-12-19 19:30:05 -0500

    trying to establish a graphql connection

---
+++ Develop/client/build/precache-manifest.e28811cf38b869a5d4c995affc33b998.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "caee76e3c48c666716a1245b82641d7e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "14579c8f8188f489984e",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "c3ed4398959dfcbd9e67",
+    "url": "/static/css/main.10507661.chunk.css"
+  },
+  {
+    "revision": "14579c8f8188f489984e",
+    "url": "/static/js/2.36f23b80.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.36f23b80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3ed4398959dfcbd9e67",
+    "url": "/static/js/main.51cbafea.chunk.js"
+  },
+  {
+    "revision": "dad4adfa7062cb4d7df7",
+    "url": "/static/js/runtime-main.597b7f10.js"
+  }
+]);
